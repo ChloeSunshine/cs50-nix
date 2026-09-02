07423b83d395cc2ca0499644ac331880b947adb8
Chloe <chloesunshine3@proton.me>
2026-09-02 02:08:09 -0400

    Update flake.nix

--- flake.nix
+++ flake.nix
@@ -13,7 +13,7 @@
         };
         pythonPackages = pkgs.python3Packages;
       in rec {
-        devShells.system.default = pkgs.mkShell {
+        devShells.${system}.default = pkgs.mkShell {
           name = "impurePythonEnv";
           venvDir = "./.venv";
           buildInputs = [
